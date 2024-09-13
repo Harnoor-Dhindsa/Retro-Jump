2df9e66eaf25470dd147c922cddb66e110b3cb79
Harnoor-Dhindsa <harnoordhindsa07@gmail.com>
2024-09-13 15:51:45 -0600

    change the point UI and the Camera setting + create a new menu

--- game_manager.gd
+++ game_manager.gd
@@ -6,4 +6,4 @@ var points = 0
 func add_point():
 	points += 1
 	print(points)
-	points_label.text = "Points: " + str(points)
+	points_label.text = "x" + str(points)
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,6 +1,1 @@
 extends Node
-
-
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
